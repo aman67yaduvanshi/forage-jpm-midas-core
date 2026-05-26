e4fddf54d9dd05e26354d88a2ff41abf52f024cf
Aman Yadav <er.aman677@gmail.com>
2026-05-26 19:38:06 +0530

    Initialize Skyscanner forage app with React components

---
+++ Skyscanner forage app.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { cssModules } from '@skyscanner/backpack-web/bpk-react-utils';
+import BpkText from '@skyscanner/backpack-web/bpk-component-text';
+import BpkButton from '@skyscanner/backpack-web/bpk-component-button';
+import BpkCalendar, { CALENDAR_SELECTION_TYPE } from '@skyscanner/backpack-web/bpk-component-calendar';
+import format from 'date-fns/format';
+import STYLES from './App.scss';
+
+// ... (component logic to set state and render with Bpk components)
+
